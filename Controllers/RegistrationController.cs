c07c705d638d393aed0fe62faaf9c5c23fc988d5
RathourPP <discover.rathore123@gmail.com>
2021-05-16 21:47:44 +0530

    Basic Architecture Of Application

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,53 @@
+﻿using DummyProjectApi.BusinessModel.Model;
+using DummyProjectApi.Repositories.EmployeeRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DummyProjectApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public RegistrationController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetRegistrationData()
+        {
+            try
+            {
+                return Ok(await _employeeRepository.GetEmployeesBasicInformation());
+            }
+            catch(Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<EmployeeBasicInformation>> GetRegistrationDataById(int Id)
+        {
+            try
+            {
+                var result = await _employeeRepository.GetEmployeeBasicInforamtionOnId(Id);
+                if(result==null)
+                {
+                    return NotFound();
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error");
+            }
+        }
+    }
+}
---
+++ Repositories/EmployeeRepository/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using DummyProjectApi.BusinessModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DummyProjectApi.Repositories.EmployeeRepository
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<EmployeeBasicInformation>> GetEmployeesBasicInformation();
+        Task<EmployeeBasicInformation> GetEmployeeBasicInforamtionOnId(int Id);
+        Task<EmployeeBasicInformation> RegisterEmployee(EmployeeBasicInformation employee);
+        Task<EmployeeBasicInformation> UpdateBasicInformation(EmployeeBasicInformation employee);
+        void DeleteEmployeeBasicInfo(int Id);
+    }
+}
